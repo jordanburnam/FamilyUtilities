2a0ec672724738019bbae610636f86d0837b6b84
Jordan <jordanburnam1990@icloud.com>
2015-11-10 15:52:40 -0500

    Init Commit

---
+++ FamilyUtilities/Networking/Router.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NATUPNPLib;
+using FamilyUtilities.Types;
+
+
+namespace FamilyUtilities.Networking
+{
+    public class Router
+    {
+        NATUPNPLib.UPnPNAT upnpnat;
+        NATUPNPLib.IStaticPortMappingCollection mappings;
+
+        
+
+        public Router()
+        {
+            upnpnat = new NATUPNPLib.UPnPNAT();
+            mappings = upnpnat.StaticPortMappingCollection;
+        }
+
+        public bool AddMapping(int iExternalPort, Protocol eProtocol, int iInternalPort, string sInternalClientIP, string sDescription)
+        {
+            bool bResult = false;
+            IStaticPortMapping oPortMapping = null;
+            oPortMapping = mappings.Add(iExternalPort, eProtocol.ToString(), iInternalPort, sInternalClientIP, true, sDescription);
+            if (oPortMapping != null)
+            {
+                
+            }
+            return bResult;
+        }
+
+        public bool RemoveMapping(int iExternalPort, Protocol eProtocol)
+        {
+            bool bResult = false;
+          
+            mappings.Remove(iExternalPort, eProtocol.ToString());
+         
+            return bResult;
+        }
+
+
+        public IList<MappedPort> GetMappedPorts()
+        {
+            List<MappedPort> oList = new List<MappedPort>();
+
+            foreach (NATUPNPLib.IStaticPortMapping portMapping in mappings)
+            {
+                oList.Add(new MappedPort(portMapping));
+            }
+
+            return oList;
+        }
+
+    }
+}
---
+++ FamilyUtilities/Types/MappedPort.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FamilyUtilities.Types
+{
+    public class MappedPort
+    {
+        int _iExternalPort;
+        int _iInternalPort;
+        Protocol _eProtocol;
+        string _sInternalIP;
+        string _sDescription;
+        bool _bEnabled;
+
+
+        public int ExternalPort {
+            get { return this._iExternalPort; }    
+        }
+
+        public int InternalPort
+        {
+            get { return this._iInternalPort; }
+        }
+
+        public string Protocol
+        {
+            get { return this._eProtocol.ToString(); }
+        }
+
+        public string Description
+        {
+            get { return this._sDescription; }
+        }
+
+        public bool Enabled
+        {
+            get { return this._bEnabled; }
+        }
+
+        public MappedPort(NATUPNPLib.IStaticPortMapping oPortMapping)
+        {
+             _eProtocol = (Protocol)Enum.Parse(typeof(Protocol), oPortMapping.Protocol);
+            _iExternalPort = oPortMapping.ExternalPort;
+            _iInternalPort = oPortMapping.InternalPort;
+            _sInternalIP = oPortMapping.InternalClient;
+          
+            _sDescription = oPortMapping.Description;
+            _bEnabled = oPortMapping.Enabled;
+        }
+        public MappedPort(string sInternalIP, int iExternalPort, int iInternalPort, Protocol eProtocol, string sDescription, bool bEnabled)
+        {
+            _iExternalPort = iExternalPort;
+            _iInternalPort = iInternalPort;
+            _sInternalIP = sInternalIP;
+            _eProtocol = eProtocol;
+            _sDescription = sDescription;
+            _bEnabled = bEnabled;
+        }
+
+        public override string ToString()
+        {
+            string sString = "";
+            sString = this._sDescription;
+
+            return sString;
+        }
+    }
+}
